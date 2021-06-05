622c77340c4e8890a0c9af171398415e0918b87c
jay_wu <jay_wu>
2021-06-05 11:30:28 +0800

    create webscoket to echo message

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chat Server
+
+ref: [Creating a WebSocket chat](https://ktor.io/docs/creating-web-socket-chat.html)
+
+## Testing
+
+[Echo Test](https://www.websocket.org/echo.html)
+
+- `ws://localhost:8080/chat`
--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,26 @@
 package com.example
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.routing.*
+import io.ktor.websocket.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(WebSockets)
+    routing {
+        webSocket("/chat") {
+            send("You are connected!")
+            for(frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                send("You said: $receivedText")
+            }
+        }
+    }
 }
+
 
